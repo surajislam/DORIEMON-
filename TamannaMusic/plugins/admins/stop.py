947cd56443a6c5a543abb1deafd0757c595d25ae
Silentsmile29 <159234561+Silentsmile29@users.noreply.github.com>
2024-03-11 00:45:56 +0530

    Update stop.py

--- TamannaMusic/plugins/admins/stop.py
+++ TamannaMusic/plugins/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from TamannaMusic import app
-from TamannaMusic.core.call import VIP
+from TamannaMusic.core.call import Tamanna
 from TamannaMusic.utils.database import set_loop
 from TamannaMusic.utils.decorators import AdminRightsCheck
 from TamannaMusic.utils.inline import close_markup
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await VIP.stop_stream(chat_id)
+    await Tamanna.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
