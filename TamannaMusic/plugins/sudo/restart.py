948fbc9d093d3d62470ea176ad8772f67fb2dca3
Silentsmile29 <159234561+Silentsmile29@users.noreply.github.com>
2024-03-11 01:17:13 +0530

    Update restart.py

--- TamannaMusic/plugins/sudo/restart.py
+++ TamannaMusic/plugins/sudo/restart.py
@@ -21,7 +21,7 @@ from TamannaMusic.utils.database import (
     remove_active_video_chat,
 )
 from TamannaMusic.utils.decorators.language import language
-from TamannaMusic.utils.pastebin import VIPBin
+from TamannaMusic.utils.pastebin import TamannaBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -116,7 +116,7 @@ async def update_(client, message, _):
     _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
     _final_updates_ = _update_response_ + updates
     if len(_final_updates_) > 4096:
-        url = await VIPBin(updates)
+        url = await TamannaBin(updates)
         nrs = await response.edit(
             f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
         )
