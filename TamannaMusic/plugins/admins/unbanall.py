01f2a83f8581dab145fc26421146ad2d01e1657c
Silentsmile29 <159234561+Silentsmile29@users.noreply.github.com>
2024-03-11 00:51:35 +0530

    Update unbanall.py

--- TamannaMusic/plugins/admins/unbanall.py
+++ TamannaMusic/plugins/admins/unbanall.py
@@ -2,7 +2,7 @@ from TamannaMusic import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from TamannaMusic.utils.vip_ban import admin_filter
+from TamannaMusic.utils.Tamanna_ban import admin_filter
 
 BOT_ID = app.id
 
