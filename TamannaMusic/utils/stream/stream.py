07f8c6dac25718777ab4615437fdf2fbba5f120b
Silentsmile29 <159234561+Silentsmile29@users.noreply.github.com>
2024-03-11 22:02:59 +0530

    Update stream.py

--- TamannaMusic/utils/stream/stream.py
+++ TamannaMusic/utils/stream/stream.py
@@ -31,6 +31,9 @@ async def stream(
 ):
     if not result:
         return
+    if video:
+        if not await is_video_allowed(chat_id):
+            raise AssistantErr(_["play_7"])
     if forceplay:
         await Tamanna.force_stop_stream(chat_id)
     if streamtype == "playlist":
@@ -46,7 +49,9 @@ async def stream(
                     duration_sec,
                     thumbnail,
                     vidid,
-                ) = await YouTube.details(search, False if spotify else True)
+                ) = await YouTube.details(
+                    search, False if spotify else True
+                )
             except:
                 continue
             if str(duration_min) == "None":
@@ -67,7 +72,7 @@ async def stream(
                 )
                 position = len(db.get(chat_id)) - 1
                 count += 1
-                msg += f"{count}. {title[:70]}\n"
+                msg += f"{count}- {title[:70]}\n"
                 msg += f"{_['play_20']} {position}\n\n"
             else:
                 if not forceplay:
@@ -80,11 +85,7 @@ async def stream(
                 except:
                     raise AssistantErr(_["play_14"])
                 await Tamanna.join_call(
-                    chat_id,
-                    original_chat_id,
-                    file_path,
-                    video=status,
-                    image=thumbnail,
+                    chat_id, original_chat_id, file_path, video=status
                 )
                 await put_queue(
                     chat_id,
@@ -98,14 +99,14 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                img = await get_thumb(vidid)
-                button = stream_markup(_, chat_id)
+                img = await gen_thumb(vidid)
+                button = stream_markup(_, vidid, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
                     caption=_["stream_1"].format(
+                        title[:27],
                         f"https://t.me/{app.username}?start=info_{vidid}",
-                        title[:23],
                         duration_min,
                         user_name,
                     ),
@@ -116,13 +117,15 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await TamannaBin(msg)
+            link = await Tamannabin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
             else:
                 car = msg
-            carbon = await Carbon.generate(car, randint(100, 10000000))
+            carbon = await Carbon.generate(
+                car, randint(100, 10000000)
+            )
             upl = close_markup(_)
             return await app.send_photo(
                 original_chat_id,
@@ -135,7 +138,6 @@ async def stream(
         vidid = result["vidid"]
         title = (result["title"]).title()
         duration_min = result["duration_min"]
-        thumbnail = result["thumb"]
         status = True if video else None
         try:
             file_path, direct = await YouTube.download(
@@ -156,21 +158,21 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            button = aq_markup(_, chat_id)
-            await app.send_message(
-                chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+            qimg = await gen_qthumb(vidid)
+            button = queue_markup(_, vidid, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=qimg,
+                caption=_["queue_4"].format(
+                    position, title[:27], duration_min, user_name
+                ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
             await Tamanna.join_call(
-                chat_id,
-                original_chat_id,
-                file_path,
-                video=status,
-                image=thumbnail,
+                chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
                 chat_id,
@@ -184,14 +186,14 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            img = await gen_thumb(vidid)
+            button = stream_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
+                    title[:27],
                     f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
                     duration_min,
                     user_name,
                 ),
@@ -216,16 +218,18 @@ async def stream(
                 "audio",
             )
             position = len(db.get(chat_id)) - 1
-            button = aq_markup(_, chat_id)
             await app.send_message(
-                chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
-                reply_markup=InlineKeyboardMarkup(button),
+                original_chat_id,
+                _["queue_4"].format(
+                    position, title[:30], duration_min, user_name
+                ),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Tamanna.join_call(chat_id, original_chat_id, file_path, video=None)
+            await Tamanna.join_call(
+                chat_id, original_chat_id, file_path, video=None
+            )
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -238,12 +242,12 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
-                caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
+                caption=_["stream_3"].format(
+                    title, duration_min, user_name
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
@@ -268,16 +272,18 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            button = aq_markup(_, chat_id)
             await app.send_message(
-                chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
-                reply_markup=InlineKeyboardMarkup(button),
+                original_chat_id,
+                _["queue_4"].format(
+                    position, title[:30], duration_min, user_name
+                ),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Tamanna.join_call(chat_id, original_chat_id, file_path, video=status)
+            await Tamanna.join_call(
+                chat_id, original_chat_id, file_path, video=status
+            )
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -292,11 +298,15 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
-                photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
-                caption=_["stream_1"].format(link, title[:23], duration_min, user_name),
+                photo=config.TELEGRAM_VIDEO_URL
+                if video
+                else config.TELEGRAM_AUDIO_URL,
+                caption=_["stream_4"].format(
+                    title, link, duration_min, user_name
+                ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
@@ -305,8 +315,7 @@ async def stream(
         link = result["link"]
         vidid = result["vidid"]
         title = (result["title"]).title()
-        thumbnail = result["thumb"]
-        duration_min = "Live Track"
+        duration_min = "ʟɪᴠᴇ"
         status = True if video else None
         if await is_active_chat(chat_id):
             await put_queue(
@@ -321,11 +330,11 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            button = aq_markup(_, chat_id)
             await app.send_message(
-                chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
-                reply_markup=InlineKeyboardMarkup(button),
+                original_chat_id,
+                _["queue_4"].format(
+                    position, title[:30], duration_min, user_name
+                ),
             )
         else:
             if not forceplay:
@@ -334,11 +343,7 @@ async def stream(
             if n == 0:
                 raise AssistantErr(_["str_3"])
             await Tamanna.join_call(
-                chat_id,
-                original_chat_id,
-                file_path,
-                video=status,
-                image=thumbnail if thumbnail else None,
+                chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
                 chat_id,
@@ -352,14 +357,14 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            img = await gen_thumb(vidid)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
+                    title[:27],
                     f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
                     duration_min,
                     user_name,
                 ),
@@ -369,8 +374,8 @@ async def stream(
             db[chat_id][0]["markup"] = "tg"
     elif streamtype == "index":
         link = result
-        title = "ɪɴᴅᴇx ᴏʀ ᴍ3ᴜ8 ʟɪɴᴋ"
-        duration_min = "00:00"
+        title = "Index or M3u8 Link"
+        duration_min = "URL stream"
         if await is_active_chat(chat_id):
             await put_queue_index(
                 chat_id,
@@ -383,10 +388,10 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            button = aq_markup(_, chat_id)
             await mystic.edit_text(
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
-                reply_markup=InlineKeyboardMarkup(button),
+                _["queue_4"].format(
+                    position, title[:30], duration_min, user_name
+                )
             )
         else:
             if not forceplay:
@@ -408,7 +413,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
