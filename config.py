ba248cfdacd913a54faf841fcf96989f9741a312
surajislam <154444093+surajislam@users.noreply.github.com>
2024-05-28 19:52:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/surajislam/DORIEMON-",
+    "https://github.com/surajislam/DORIEMON",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
